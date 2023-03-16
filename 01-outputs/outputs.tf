e2c2edbe75bca8814b889aec9a9dab2974a43c4b
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-03-16 17:08:27 +0530

    updating ./learning-terraform

--- 01-outputs/outputs.tf
+++ 01-outputs/outputs.tf
@@ -6,5 +6,6 @@ output "sample01" {
   value = "Hello world"
 }
 
+#Output prints the data on the screen.
 #Every block needs a key.It helps in declaring multiple outputs.
 #Every resource in the terraform has name.
