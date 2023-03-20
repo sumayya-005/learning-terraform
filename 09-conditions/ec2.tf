d72cbee15d3b08eaec32554580fc8a3cb5e597fb
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-03-20 07:07:40 +0530

    updating ./learning-terraform

--- 09-conditions/ec2.tf
+++ 09-conditions/ec2.tf
@@ -8,3 +8,57 @@ resource "aws_instance" "sample" {
   }
 }
 
+provisioner "remote-exec" {
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  inline = [
+    "yum install nginx -y",
+    "systemctl start nginx "
+  ]
+
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+count       = var.create_sg ? 1 : 0
+name        = "allow_ssh"
+description = "Allow TLS inbound traffic"
+vpc_id      = "vpc-075435ce9af088bfa"
+
+ingress {
+description = "TLS from VPC"
+from_port   = 22
+to_port     = 22
+protocol    = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+description = "TLS from VPC"
+from_port   = 80
+to_port     = 80
+protocol    = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+egress {
+from_port        = 0
+to_port          = 0
+protocol         = "-1"
+cidr_blocks      = ["0.0.0.0/0"]
+ipv6_cidr_blocks = ["::/0"]
+}
+
+tags = {
+Name = "allow_ssh"
+}
+}
+
+variable "create_sg" {
+default = true
+}
